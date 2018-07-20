35506b072fbbb4496c1ce66815cbea42670bf90f
darthbeeblebrox <charles.fieseler@gmail.com>
2018-07-19 21:19:05 -0700

    Calculate true (recursive) size of complex objects; dependent properties not counted

---
+++ miscellaneous/get_size_recursive.m
@@ -0,0 +1,56 @@
+function [ bytes ] = get_size_recursive( var, verbose )
+% Non-recursive version from: 
+% https://www.mathworks.com/matlabcentral/answers/14837-how-to-get-size-of-an-object
+if ~exist('verbose','var')
+    verbose = 2;
+end
+bytes = 0;
+warning off 'MATLAB:structOnObject'
+if iscell(var)
+    if (verbose==2)||(verbose==3)
+        fprintf('  ')
+    end
+    for i=1:length(var)
+        bytes = bytes + get_size_recursive(var{i}, verbose);
+    end
+    if ((verbose==2)||(verbose==3))&&bytes>0
+        fprintf('Size of this cell array (length=%d) is %d\n',...
+            length(var), bytes)
+    end
+elseif isstruct(var) || isobject(var)
+    props = fieldnames(struct(var));
+    % Note: this will only skip non-private dependent properties
+    dependent_props = findAttrValue(var,'Dependent');
+    if ((verbose==2)||(verbose==3))&&bytes>0
+        fprintf('  ')
+    end
+    for ii=1:length(props)
+        if ismember(char(props(ii)), dependent_props)
+            continue
+        end
+        try
+            currentProp = var.(char(props(ii)));
+            bytes = bytes + get_size_recursive(currentProp, verbose);
+        catch
+            if (verbose==1)||(verbose==3)
+                fprintf('Treating inaccessible property %s as empty\n',...
+                    char(props(ii)))
+            end
+        end
+    end
+    if ((verbose==2)||(verbose==3))&&bytes>0
+        fprintf('Size of this %s is %d\n',...
+            class(var), bytes)
+    end
+else
+    bytes = whos(varname(var)); 
+    bytes = bytes.bytes;
+end
+
+warning on 'MATLAB:structOnObject'
+
+function [ name ] = varname( ~ )
+    name = inputname(1);
+end
+
+end
