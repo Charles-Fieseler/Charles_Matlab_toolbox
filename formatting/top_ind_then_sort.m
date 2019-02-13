2b81b47fee69eb40dc0beddfcb4ed009288eaec0
darthbeeblebrox <charles.fieseler@gmail.com>
2019-02-13 11:01:41 -0800

    Function for taking only the top indices of a vector, and then doing some sorting

---
+++ formatting/top_ind_then_sort.m
@@ -0,0 +1,28 @@
+function [ordered_dat, ordered_ind] = top_ind_then_sort(...
+    dat, top_x, threshold)
+% Takes the largest values of the absolute value of the dataset, then
+% resorts them by their signed value
+%
+% Input:
+%   dat - the data matrix
+%   top_x - the top x to take
+%   threshold - the cutoff value
+% NOTE: ONE of top_x or threshold must be passed; top_x dominates
+assert(exist('top_x', 'var') || exist('threshold', 'var'),...
+    'Pass an integer or a threshold')
+
+% Only get the interesting data
+all_rows = sum(abs(dat),2);
+if exist('top_x', 'var') && ~isempty(top_x)
+    [~, interesting_ind] = sort(all_rows, 'descend');
+    ordered_ind = interesting_ind(1:top_x);
+else
+    ordered_ind = find(all_rows > threshold);
+end
+% Re-sort it
+[~, sort_ind] = sort(dat(ordered_ind,1), 'ascend');    
+ordered_ind = ordered_ind(sort_ind);
+ordered_dat = dat(ordered_ind, :);
+
+end
+
