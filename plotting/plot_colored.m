5598be16b542e7a4a7cc02b88f1d02970721cea7
Charles-Fieseler <konda@uw.edu>
2020-03-24 16:39:06 -0700

    Try to add inputs back in to be the same as the accidentally deleted one

--- plotting/plot_colored.m
+++ plotting/plot_colored.m
@@ -1,4 +1,4 @@
-function [fig] = plot_colored( dat, categories, legend_str, plot_options )
+function [fig] = plot_colored( dat, categories, legend_str, plot_options, cmap, fig )
 % Plots a 3d trace of data colored by the given label indices
 %   If there are more than 8 categories, then the later ones are all black
 if ~exist('plot_options','var')
@@ -19,22 +19,33 @@ else
 end
 
 num_states = length(unique(categories));
-cmap = lines(num_states);
-if num_states>7
-    % Makes the 8th and later categories black (otherwise would repeat)
-    cmap(8:end,:) = zeros(size(cmap(8:end,:)));
+if ~exist('cmap', 'var')
+    cmap = lines(num_states);
+    if num_states>7
+        % Makes the 8th and later categories black (otherwise would repeat)
+        cmap(8:end,:) = zeros(size(cmap(8:end,:)));
+    end
+elseif num_states > size(cmap,1)
+    % Make sure there are enough colors
+    n = size(cmap,1);
+    cmap(n+1:num_states, :) = zeros(num_states-n,3);
 end
-fig = figure('DefaultAxesFontSize',12);
+if ~exist('fig', 'var')
+    fig = figure('DefaultAxesFontSize',12);
+end
 hold on
 f = gca;
 for jC = unique(categories)
     ind = find(categories==jC);
-    if ~contains(plot_options,'.')
+    if contains(plot_options,'plot')
+        plot3(x(ind), y(ind), z(ind), 'color', cmap(jC,:));
+    elseif ~contains(plot_options,'.')
         scatter3(x(ind), y(ind), z(ind), 100, plot_options, 'Filled');
+    f.Children(1).CData = cmap(jC,:);
     else
         scatter3(x(ind), y(ind), z(ind), 100, plot_options);
-    end
     f.Children(1).CData = cmap(jC,:);
+    end
 end
 xlabel('1st mode')
 ylabel('2nd mode')
