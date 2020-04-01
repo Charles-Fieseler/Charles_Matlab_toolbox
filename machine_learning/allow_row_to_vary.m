daeaf6d315e0f515dd62c86732e5dccdb992a35f
Charles-Fieseler <konda@uw.edu>
2020-03-31 18:02:59 -0700

    Refactor for use with ControlSignalPath objects more explicitly; accepts function string

--- machine_learning/allow_row_to_vary.m
+++ machine_learning/allow_row_to_vary.m
@@ -1,7 +1,7 @@
 function [hyper_best_U,hyper_all_err,hyper_min_1se_ind,hyper_min_1se_err] =...
     allow_row_to_vary(...
     control_signal_path, num_error_steps, initial_ind, lambda, k, ...
-    num_outer_iterations, cap_at_0)
+    num_outer_iterations, cap_at_0, min_func, objective_func_str)
 % For metrics that were calculated as a global minimum, refine by keeping
 % all but one row fixed, and optimize over that row. Made for use with the
 % dmdc_cross_val.m function that calculates multiple time step errors at
@@ -15,17 +15,49 @@ function [hyper_best_U,hyper_all_err,hyper_min_1se_ind,hyper_min_1se_err] =...
 %   lambda (1) - a free parameter multiplying the standard error to determine
 %       the threshold for letting sparse parameters in
 %   k (5) - number of folds
-if ~exist('lambda', 'var')
+%   num_outer_iterations (1) - number of iterations to run the row-varying
+%       algorithm, allowing pruning to remove rows in between iterations
+%   cap_at_0 (false) - whether to cap the threshold at 0 error
+%   min_func (@last_local_min) - the function used for determining the
+%       sparsity. The global minimum is generally unstable and too dense,
+%       so by default all local minima are found, and the sparsest one that
+%       is good, within lambda*(standard error of the global min), is used
+%   objective_func_str ('cv') - the objective function to minimize. Default
+%       is cross validation 
+
+if ~exist('lambda', 'var') || isempty(lambda)
     lambda = 1;
 end
-if ~exist('k', 'var')
+if ~exist('k', 'var') || isempty(k)
     k = 5;
 end
-if ~exist('cap_at_0', 'var')
+if ~exist('cap_at_0', 'var') || isempty(cap_at_0)
     cap_at_0 = false;
 end
+if ~exist('min_func', 'var') || isempty(min_func)
+    min_func = @last_local_min;
+end
+if ~exist('objective_func_str', 'var')
+    objective_func_str = 'cv';
+end
 
+% Set up objective function
 local_path = copy(control_signal_path);
+% Renaming for later interpretability
+X = local_path.data;
+
+switch objective_func_str
+    case 'cv'
+        objective_func = @(U) ...
+            dmdc_cross_val(X, U, k, num_error_steps, [], false);
+    case 'aic'
+        objective_func = @(U) ...
+            aic_2step_dmdc(X, U, [], [], num_err_steps, false, ...
+                'standard');
+    otherwise
+        error('Unrecognized objective function')
+end
+
 % Variables for later sizes
 num_folds = k - 1;
 num_alg_iters = length(local_path.all_U);
@@ -35,8 +67,6 @@ num_hyper = length(num_error_steps);
 ind_to_test = 1:num_alg_iters;
 
 
-% Renaming for later interpretability
-X = local_path.data;
 
 warning off
 for iOuter = 1:num_outer_iterations
@@ -56,16 +86,22 @@ for iOuter = 1:num_outer_iterations
     for iRow = 1:size(base_U, 1)
         % Loop through each control signals channel (row)
         all_err = zeros(length(ind_to_test), num_hyper, num_folds);
-        for iIter = 1:length(ind_to_test)
-            % Retry all sparsity possibilities, leaving other rows same
-            this_U = base_U;
-            this_row_i = ind_to_test(iIter);
-            new_row_U = local_path.all_U{this_row_i};
-            this_U(iRow,:) = new_row_U(iRow,:);
-            [~, all_err(iIter,:,:)] = ...
-                dmdc_cross_val(X, this_U, k, num_error_steps, ...
-                [], false);
-        end
+%         for iIter = 1:length(ind_to_test)
+%             % Retry all sparsity possibilities, leaving other rows same
+%             this_U = base_U;
+%             this_row_i = ind_to_test(iIter);
+%             new_row_U = local_path.all_U{this_row_i};
+%             this_U(iRow,:) = new_row_U(iRow,:);
+% %             [~, all_err(iIter,:,:)] = ...
+% %                 dmdc_cross_val(X, this_U, k, num_error_steps, ...
+% %                 [], false);
+%             [~, all_err(iIter,:,:)] = ...
+%                 objective_func(X, this_U, num_error_steps);
+%         end
+        out = local_path.iterate_single_row(...
+                base_U, ind_to_test, iRow, objective_func);
+        all_err = flatten_cell_array(out); % TODO
+        error();
         % TEST: subtract the mean instead of the abolute errors to better
         % compare folds
         all_err = all_err - mean(all_err,1);
@@ -74,7 +110,7 @@ for iOuter = 1:num_outer_iterations
         tmp_best_ind = zeros(sz(2), 1);
         tmp_best_val = zeros(sz(2), 1);
         for i = 1:sz(2)
-            [tmp_best_ind(i), tmp_best_val(i)] = last_local_min(...
+            [tmp_best_ind(i), tmp_best_val(i)] = min_func(...
                 squeeze(all_err(:,i,:)), lambda, false, cap_at_0);
         end
 %         [~, tmp_min_ind] = min(all_err_means);
