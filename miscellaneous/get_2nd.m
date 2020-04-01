bf9e2965397c9fcf1f15bdec7aad65d707939e1d
Charles-Fieseler <konda@uw.edu>
2020-04-01 16:25:11 -0700

    docs

--- miscellaneous/get_2nd.m
+++ miscellaneous/get_2nd.m
@@ -1,5 +1,7 @@
 function [out] = get_2nd(func, varargin)
 % Gets 2nd output from a function that returns two values with no input
+% For example, to get just the index of a min/max function:
+%   ind = get_2nd(@min,dat);
 [~, out] = func(varargin{:});
 end
 
